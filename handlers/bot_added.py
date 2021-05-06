97b3df87f66d6fba139efa84148c52cdd925b3be
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-05-06 11:22:01 +0530

    Create bot_added.py

---
+++ handlers/bot_added.py
@@ -0,0 +1,13 @@
+from . import *
+from pyrogram import *
+from pyrogram.types import *
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(client, message):
+    try:
+        joiner = await Client.get_me() 
+        for user in message.new_chat_members:
+            if int(joiner.id) == int(user.id):
+                await message.reply_text("Thanks for adding me to your Group :) \nPromote me now")
+    except Exception as e:
+        await Client.send_message(int("1205330319"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
