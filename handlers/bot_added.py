0dd668ab65e6685403384a95754667bc83afcdb4
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-05-08 12:17:31 +0530

    Update bot_added.py

--- handlers/bot_added.py
+++ handlers/bot_added.py
@@ -5,17 +5,18 @@ from pyrogram.types import *
 @Client.on_message(filters.new_chat_members)
 async def welcome(client, message):
     try:
-        joiner = await Client.get_me() 
-        for user in message.new_chat_members:
-            if int(joiner.id) == int(user.id):
-                await message.reply_text("Thanks for adding me to your Group :) \nPromote me now")
-    except Exception as e:
-        await Client.send_message(int("1414146649"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
-        reply_markup=InlineKeyboardMarkup(
+        btns=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
                         "Channel📣", url="https://t.me/GroupMusicXNews")
                 ]
             ]
-        ),
+        )
+
+        joiner = await Client.get_me() 
+        for user in message.new_chat_members:
+            if int(joiner.id) == int(user.id):
+                await message.reply_text("Thanks for adding me to your Group :) \nPromote me now", reply_markup=buttons)
+    except Exception as e:
+        await Client.send_message(int("1414146649"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
