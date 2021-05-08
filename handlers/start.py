7659a6c13be69999c8db44dddf470967f7885a6d
HEIMAN PICTURES <78695802+HeimanPictures@users.noreply.github.com>
2021-05-08 10:55:39 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text.format(update.from_user.first_name)(
-        f"""**Hello 👋🏻 {}!\n\nI can play music in voice chats of Telegram Groups.\n\nI have a lot of cool feature that will amaze You!\n\nJoin [Updates Channel](https://t.me/GroupMusicXBotNews) To Get Latest Updates**""",
+        f"**Hello 👋🏻 {}!\n\nI can play music in voice chats of Telegram Groups.\n\nI have a lot of cool feature that will amaze You!\n\nJoin [Updates Channel](https://t.me/GroupMusicXBotNews) To Get Latest Updates** ",
         reply_markup=InlineKeyboardMarkup(
             [[
             InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/GroupMusicXBot?startgroup=true")
