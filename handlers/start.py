2d8e8bf7614e849a61862d6469aebe3fcff5183e
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-09 17:17:17 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,33 +8,28 @@ from config import BOT_NAME as bn
 
 @Client.on_message(filters.command(["start", "start@GroupMusicXBot"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
-    await message.reply_text("**Hello 👋🏻 {}!\n\nI can play music in voice chats of Telegram Groups.\n\nI have a lot of cool feature that will amaze You!\n\nJoin [Updates Channel](https://t.me/GroupMusicXNews) To Get Latest Updates**".format(message.from_user.mention),
+    await message.reply_text(
+        text="**Hello 👋🏻 {}!\n\nI can play music in voice chats of Telegram Groups.\n\nI have a lot of cool feature that will amaze You!\n\nJoin [Updates Channel](https://t.me/GroupMusicXNews) To Get Latest Updates**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
-          [
-            [
+            [[
             InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/GroupMusicXBot?startgroup=true")
-            ],
-            [
+            ],[
             InlineKeyboardButton("💬 Group", url="https://t.me/MusicBotSupports"),
-            InlineKeyboardButton("Channel 📣", url="https://t.me/GroupMusicXNews"),
-            ],
-            [
+            InlineKeyboardButton("Channel 📣", url="https://t.me/GroupMusicXNews")
+            ],[
             InlineKeyboardButton("🎛 Commands 🎛", url="https://telegra.ph/Music-Bot-05-07")
-            ]
-          ]
+            ]]
         ),
         disable_web_page_preview=True
     )
 
 @Client.on_message(filters.command(["start", "start@GroupMusicXBot"]) & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**Music player is online ✅**""",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
-                ]
-            ]
-        )
-   )
+      await message.reply_text(
+          text="**Music player is online ✅**",
+          reply_markup=InlineKeyboardMarkup(
+              [[
+              InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
+              ]]
+          )
+      )
