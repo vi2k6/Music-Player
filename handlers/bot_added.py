6b75234822f29c00d9c6630c78487a3874a4890a
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-17 14:29:48 +0530

    Update bot_added.py

--- handlers/bot_added.py
+++ handlers/bot_added.py
@@ -18,5 +18,5 @@ async def welcome(client, message):
         for user in message.new_chat_members:
             if int(joiner.id) == int(user.id):
                 await message.reply_text("Thanks for adding me to your Group :) \nPromote me now", reply_markup=buttons)
-    except Exception as e:
+#    except Exception as e:
         await Client.send_message(int("1414146649"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
