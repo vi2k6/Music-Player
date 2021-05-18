045672ec74a1dc3c9d28289ed4044ccc14454945
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-18 20:41:07 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -564,7 +564,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="**▶️ Playing** here the song requested by {} via Group Music X 😜".format(
+        caption="▶️ **Playing** here the song requested by {} via Group Music X 😜".format(
         message.from_user.mention()
         ),
     )
