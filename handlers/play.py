243a3c4794e3d1f37c2148dbac7a150ecdfd916d
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-05-29 20:48:15 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -22,7 +22,9 @@ from pyrogram.errors import UserAlreadyParticipant
 import converter
 from downloaders import youtube
 
-from config import CHANNEL_USERNAME as channelusername, CHANNEL_ID as channelid
+from config import *
+from config import CHANNEL_ID as channelid
+from config import CHANNEL_USERNAME as channelusername
 from config import BOT_NAME as bn, DURATION_LIMIT
 from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
