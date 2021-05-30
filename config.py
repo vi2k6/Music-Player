7e83bdd471cea4ae4067265089b9310a82273358
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-05-30 12:25:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,18 +22,18 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_NAME = getenv("BOT_NAME", "")
 admins = {}
 API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_HASH = getenv("API_HASH", "")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "")
 
-CHANNEL_USERNAME = getenv("CHANNEL_USERNAME")
+CHANNEL_USERNAME = getenv("CHANNEL_USERNAME", "")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
