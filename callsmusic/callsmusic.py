7eba51d925076b2e419573bbb421c92c23d73689
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-07 11:17:48 +0530

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -19,7 +19,7 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-import config
+from . import config
 from . import queues
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
