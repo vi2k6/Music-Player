acfd8e0fd85cb663db0b09375a0058bbc88291dd
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-11 18:32:10 +0530

    Update bot_added.py

--- handlers/bot_added.py
+++ handlers/bot_added.py
@@ -15,7 +15,7 @@ async def welcome(client, message):
         for user in message.new_chat_members:
             if int(joiner.id) == int(user.id):
                 await message.reply_text("Thanks for adding me to your Group :) \nPromote me now", reply_markup=buttons)
-    except Exception as e:
-        await Client.send_message(int("1711651694"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
+ # except Exception as e:
+     # await Client.send_message(int("1711651694"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
 
 # It's Works?
