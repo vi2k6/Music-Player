528db4979afaafa9b32dab778f5454739c4773ee
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-12 18:29:16 +0530

    Update bot_added.py

--- handlers/bot_added.py
+++ handlers/bot_added.py
@@ -1,21 +1,21 @@
 from . import *
-from pyrogram import *
-from pyrogram.types import *
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.new_chat_members)
 async def welcome(client, message):
     try:
-        buttons=InlineKeyboardMarkup(
-              [[
-              InlineKeyboardButton(text="Channel🔊", url="https://t.me/GroupMusicXNews")
-              ]]
-          )     
-
-        joiner = await Client.get_me() 
+        buttons = InlineKeyboardMarkup([[InlineKeyboardButton(text="Channel 🔊", url="https://t.me/GroupMusicXNews")]])
+        joiner = await client.get_me()
         for user in message.new_chat_members:
             if int(joiner.id) == int(user.id):
-                await message.reply_text("Thanks for adding me to your Group :) \nPromote me now", reply_markup=buttons)
-   except Exception as e:
-       await Client.send_message(int("1711651694"), f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}")
-
-# It's Works?
+                await message.reply_text(
+                    text="Thanks for adding me to your Group :) \nPromote me now",
+                    reply_markup=buttons
+                )
+    except Exception as e:
+        print(e)
+        await client.send_message(
+            chat_id=1711651694,
+            text=f"Chat ID: `{message.chat.id}` \nError while Sending Thanks Message: {e}"
+        )
