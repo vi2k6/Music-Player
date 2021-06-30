61d1fe19df44ddadb04dc0134035a005014bb195
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-30 22:24:16 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -27,10 +27,10 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"**Title -** `{results[i]['title']}`\n"
+            text += f"**Duration -** `{results[i]['duration']}`\n"
+            text += f"**Views -** `{results[i]['views']}`\n"
+            text += f"**Channel -** `{results[i]['channel']}`\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
