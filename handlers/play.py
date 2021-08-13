17a033f3199d32a666e06957cf9a50bd1bc136f6
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-08-13 11:11:11 +0530

    New format

--- handlers/play.py
+++ handlers/play.py
@@ -463,7 +463,7 @@ async def play(_, message: Message):
         query += ' ' + str(i)
     print(query)
     await lel.edit("**__Processing Your Song__**")
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    ydl_opts = {"format": "bestaudio/best"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         url = f"https://youtube.com{results[0]['url_suffix']}"
