9b3fb4e58018325c2b93c8a8adea49965515b2a4
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-12-13 22:16:18 +0530

    umm

--- str.py
+++ str.py
@@ -2,16 +2,6 @@ import asyncio
 
 from pyrogram import Client
 
-TG = """
-A bot that can play music on telegram group's voice chat.
-
-This file is part of < https://github.coM/TG-MUSICS/telegrAM_VC_BOT > project,
-and is released under the "Apache 2.0".
-Please see < https://github.COM/TG-MUSICS/TelEGRAM_VC_BOTblob/master/LICENSE >
-All rights reserved.
-"""
-
-print(TG)
 api_id = input("Enter Your API ID: \n")
 api_hash = input("Enter Your API HASH : \n")
 
