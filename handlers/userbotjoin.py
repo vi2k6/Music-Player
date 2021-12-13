a690eb95d39d2d0d30396e6e466dd5ba7276423a
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-12-13 22:26:01 +0530

    umm

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -13,6 +13,8 @@ async def addchannel(client, message):
     chid = message.chat.id
     try:
         invitelink = await client.export_chat_invite_link(chid)
+        if invitelink.startswith("https://t.me/+"):
+            invitelink = invitelink.replace("https://t.me/+","https://t.me/joinchat/")
     except:
         await message.reply_text(
             "<b>Add me as admin of yor group first</b>",
