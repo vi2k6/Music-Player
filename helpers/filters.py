481ebd614caa050c7136e3ce768c3714f76b417f
Vivek Tp <Vivektvp1@gmail.com>
2023-03-03 18:33:19 +0530

    PyrogramV2 Updated
    Edited Attribute Is no more With PyrogramV2.

--- helpers/filters.py
+++ helpers/filters.py
@@ -21,8 +21,8 @@ from pyrogram import filters
 
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+other_filters = filters.group & ~ filters.via_bot & ~ filters.forwarded
+other_filters2 = filters.private & ~ filters.via_bot & ~ filters.forwarded
 
 
 def command(commands: Union[str, List[str]]):
